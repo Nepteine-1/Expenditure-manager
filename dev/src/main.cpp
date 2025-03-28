e086a72217b09beef1b3b1d65cafba8e493c4036
Nepteine-1 <nepteine@gmail.com>
2025-03-28 18:38:42 +0100

    (test) add google_test to the project to automate unit tests

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -2,19 +2,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <custom_db.h>
+#include <gtest/gtest.h>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    qmlRegisterType<CustomDB>("CustomComponents",1,0,"CustomDB");
-    QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("Expenditure-manager", "Main");
+    #ifdef TEST_BUILD
+        testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    #else
+        QApplication app(argc, argv);
+        qmlRegisterType<CustomDB>("CustomComponents",1,0,"CustomDB");
+        QQmlApplicationEngine engine;
+        QObject::connect(
+            &engine,
+            &QQmlApplicationEngine::objectCreationFailed,
+            &app,
+            []() { QCoreApplication::exit(-1); },
+            Qt::QueuedConnection);
+        engine.loadFromModule("Expenditure-manager", "Main");
 
-    return app.exec();
+        return app.exec();
+    #endif
 }
---
+++ test/test_custom_db.cpp
@@ -0,0 +1,6 @@
+#include <dev/include/custom_db.h>
+#include <gtest/gtest.h>
+
+TEST(InitTest, Simple_equality) {
+    EXPECT_EQ(1,1);
+}
